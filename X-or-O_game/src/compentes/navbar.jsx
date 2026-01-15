b6990b2edc87775eb6ad04624301e8483019572e
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-15 21:00:49 +0530

    update

--- X-or-O_game/src/compentes/navbar.jsx
+++ X-or-O_game/src/compentes/navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='w-[99%] h-[99%] flex flex-col justify-center items-center bg-indigo-500'>
+    <div className='w-[99%] h-[10vh] flex flex-col justify-center items-center bg-indigo-500'>
       <div className="w-[49%] h-[99%] flex justify-start items-center">
         <h1 className='text-2xl font-bold'><li>
             <NavLink to="/" className={(e) => { return e.isActive ? "text-indigo-700 font-bold text-[18px] transition-all duration-200" : "text-[#565D6DFF] transition-all duration-200 hover:text-indigo-600" }}>PlaySphere</NavLink>
