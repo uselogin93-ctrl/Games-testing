471026066c562049dcdee7a6021a53074030c543
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-16 08:05:15 +0530

    update

--- X-or-O_game/src/compentes/XO/body.jsx
+++ X-or-O_game/src/compentes/XO/body.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useState,useEffect,useRef } from 'react'
+import { useState,useRef } from 'react'
 
 const Body = () => {
-  const [boxvalue, setboxvalue] = useState("X");
+  const [boxvalue, setboxvalue] = useState("x");
   const box1 = useRef();
   const box2 = useRef();
   const box3 = useRef();
@@ -13,14 +13,14 @@ const Body = () => {
   const handleboxclick = (e) => {
     let x="/X.png"
     let o="/O.png"
-    if(!boxvalue===x){
+    if(boxvalue===x){
       e.src=o
-      setboxvalue(x)
+      setboxvalue(o)
       // e.current.style.color="White"
       // e.current.style.backgroundColor="Black"
     }else{
       e.src=x
-      setboxvalue(o)
+      setboxvalue(x)
       // e.current.style.color="White"
       // e.current.style.backgroundColor="Black"
     }
@@ -29,22 +29,22 @@ const Body = () => {
     <div className='w-[99%] h-[99%] flex justify-center items-center bg-transparent p-2.5'>
         <div className='w-[80%] h-[70%] grid grid-cols-3 justify-center items-center bg-indigo-200 ring-2 ring-indigo-700 rounded-lg shadow-2xs shadow-indigo-700'>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box1} className='w-full h-full' onClick={handleboxclick(box1)}/>
+                <img src="" value={boxvalue} ref={box1} className='w-full h-full' onClick={() => {handleboxclick(box1.current)}}/>
             </div>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box2} className='w-full h-full' onClick={handleboxclick(box2)}/>
+                <img src="" value={boxvalue} ref={box2} className='w-full h-full' onClick={() => {handleboxclick(box2.current)}}/>
             </div>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box3} className='w-full h-full' onClick={handleboxclick(box3)}/>
+                <img src="" value={boxvalue} ref={box3} className='w-full h-full' onClick={() => {handleboxclick(box3.current)}}/>
             </div>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box4} className='w-full h-full' onClick={handleboxclick(box4)}/>
+                <img src="" value={boxvalue} ref={box4} className='w-full h-full' onClick={() => {handleboxclick(box4.current)}}/>
             </div>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box5} className='w-full h-full' onClick={handleboxclick(box5)}/>
+                <img src="" value={boxvalue} ref={box5} className='w-full h-full' onClick={() => {handleboxclick(box5.current)}}/>
             </div>
             <div className='w-[99%] h-[99%] flex justify-center items-center bg-indigo-100 ring-2 ring-indigo-700 rounded-lg shadow-xs shadow-indigo-700'>
-                <img src="" value={boxvalue} ref={box6} className='w-full h-full' onClick={handleboxclick(box6)}/>
+                <img src="" value={boxvalue} ref={box6} className='w-full h-full' onClick={() => {handleboxclick(box6.current)}}/>
             </div>
         </div>
     </div>
--- X-or-O_game/src/compentes/game.jsx
+++ X-or-O_game/src/compentes/game.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Game = () => {
   return (
     <div className='w-[99%] h-[99%] flex justify-start items-start bg-transparent p-2.5'>
-        <div className='w-75 h-100 flex flex-row justify-center items-center bg-indigo-200 ring-2 ring-indigo-700 rounded-lg shadow-2xs shadow-indigo-700 p-2.5 gap-1'>
+        <div className='w-75 h-100 flex flex-col justify-center items-center bg-indigo-200 ring-2 ring-indigo-700 rounded-lg shadow-2xs shadow-indigo-700 p-2.5 gap-1'>
             <img src="" className='w-[99%] h-[70%] ' />
             <h3 className='text-xl font-bold w-[99%] h-[10%]'>X or O</h3>
             <button className='w-[99%] h-[20%] flex justify-center items-center bg-indigo-200 ring-2 ring-indigo-400 rounded-lg shadow-xs shadow-indigo-400'><NavLink to="/xo">Play</NavLink></button>
