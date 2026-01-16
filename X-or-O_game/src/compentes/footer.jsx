6fdee50a6e5148184a7704b6a8935ded4be71b44
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-16 07:51:13 +0530

    update

--- X-or-O_game/src/compentes/footer.jsx
+++ X-or-O_game/src/compentes/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
      <div className='w-screen h-[5vh] bg-transparent flex items-center justify-center p-4 text-white italic '>
-        <p className='text-white hover:text-indigo-700 cursor-default'>© 2024 &lt;PassOP/&gt;. All rights reserved.</p>
+        <p className='text-white hover:text-indigo-700 cursor-default'>© 2024 &lt;PlaySphere/&gt;. All rights reserved.</p>
     </div>
   )
 }
